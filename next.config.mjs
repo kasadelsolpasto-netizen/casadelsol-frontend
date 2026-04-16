8f8f10848eab1a709d03bad829d2b6a99a78a60a
Daniel Caicedo <79131464+danielcaicedo95@users.noreply.github.com>
2026-04-15 20:39:55 -0500

    solucion

--- next.config.mjs
+++ next.config.mjs
@@ -27,12 +27,12 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://accounts.google.com https://checkout.wompi.co https://cdn.wompi.co https://wompi.co;
+              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://accounts.google.com https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ https://www.recaptcha.net/ https://checkout.wompi.co https://cdn.wompi.co https://wompi.co;
               style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
               img-src 'self' data: blob: https://lh3.googleusercontent.com https://res.cloudinary.com https://grainy-gradients.vercel.app https://snbqdxtrlvymswgqhxky.supabase.co;
               font-src 'self' https://fonts.gstatic.com;
-              connect-src 'self' http://localhost:3001 https://casadelsol-backend.onrender.com https://snbqdxtrlvymswgqhxky.supabase.co https://accounts.google.com https://www.googleapis.com;
-              frame-src 'self' https://accounts.google.com https://checkout.wompi.co;
+              connect-src 'self' http://localhost:3001 https://casadelsol-backend.onrender.com https://snbqdxtrlvymswgqhxky.supabase.co https://accounts.google.com https://www.googleapis.com https://www.google.com/recaptcha/ https://www.recaptcha.net/;
+              frame-src 'self' https://accounts.google.com https://www.google.com/recaptcha/ https://www.recaptcha.net/ https://checkout.wompi.co;
               upgrade-insecure-requests;
             `.replace(/\s{2,}/g, ' ').trim(),
           },
